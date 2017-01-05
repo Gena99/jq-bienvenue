4ce74927c4485e7a3eb968f46c3837bb935b2af0
Gena <gena@e64.net>
2017-01-06 00:15:18 +0100

    rendu

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('have fun !');
+$('document').ready(function(){
+	var firstName= $('#first_name').value();
+	console.log('formulaire recupere : ' + first_name);
+	//var name=$(this).attr('first_name');
+	//console.log('formulaire : ' + name);
+
+}
