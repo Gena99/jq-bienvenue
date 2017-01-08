9725532d065c4808de141ccca0fa472f3a8d0f77
Gena <gena@e64.net>
2017-01-08 12:01:27 +0100

    Formulaire ok

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 console.log('have fun !');
-$('document').ready(function(){
-	var firstName= $('#first_name').value();
-	console.log('formulaire recupere : ' + first_name);
-	//var name=$(this).attr('first_name');
-	//console.log('formulaire : ' + name);
+$(document).ready(function(){
 
-}
+// Déclarer un gestionnaire d'évènement click (event handler) sur le button créé dans le HTML
+	$('button').click(function(){
+		
+		// Déclarer une variable contact de type objet
+		var contact = {
+			firstName: $('#first_name').val(),
+			lastName: $('#last_name').val(),
+			city: $('#city').val()
+		};
+		console.log(contact);
+
+		// Changer le titre "stranger" par nom et prénom
+		$('#username').text(contact.lastName + ' ' + contact.firstName);
+	});
+});
